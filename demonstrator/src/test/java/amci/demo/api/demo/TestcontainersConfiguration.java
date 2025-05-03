5d0c7907c46627e4d463f840f7e03d8b96d51c7b
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2025-05-03 23:56:05 +0200

    Saving the current work at Sa  3 Mai 2025 23:56:05 CEST from /Users/plx/code/bp/api.d/add-vanilla-spring-boot-application/demonstrator

--- demonstrator/src/test/java/amci/demo/api/demo/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package amci.demo.api.demo;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
