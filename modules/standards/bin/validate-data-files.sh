71718f136c897eca09d0fc4320779b641a1d1c78
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2026-03-02 19:32:31 +0100

    Validate datafiles in the CI pipeline
    The datafile for HTTP related standards will
    now be validated on each pipeline run to
    ensure, that only valid changes can be merged.

--- modules/standards/bin/validate-data-files.sh
+++ modules/standards/bin/validate-data-files.sh
@@ -2,6 +2,7 @@
 
 set -e -u -o pipefail
 
+EXIT_CODE=0
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source  ${SCRIPT_DIR}/functions.sh
@@ -9,10 +10,17 @@ source  ${SCRIPT_DIR}/functions.sh
 FIELDS_TO_VALIDATE="title url status type"
 
 for FIELD_TO_VALIDATE in ${FIELDS_TO_VALIDATE}; do
-  echo "Standard missing field ${FIELD_TO_VALIDATE}:"
-  cat modules/standards/data/http-related-standards.yaml \
+  echo "Missing value for required field '${FIELD_TO_VALIDATE}':"
+  validation_result=$(cat modules/standards/data/http-related-standards.yaml \
     | convert_yaml_to_json \
-    | jq -r '[ .standards.[] ] | sort_by(.title) | map(select(.'${FIELD_TO_VALIDATE}' == null)) | "  " + .[].title'
+    | jq -r '[ .standards.[] ] | sort_by(.title) | map(select(.'${FIELD_TO_VALIDATE}' == null)) | "- " + .[].title')
 
+  if [[ ! -z ${validation_result} ]]; then
+    echo ${validation_result}
+    EXIT_CODE=1
+  fi
   echo
+
 done
+
+exit ${EXIT_CODE}
