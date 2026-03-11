215407c1428b9da6cc49a0aecee46459611bcf44
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2026-03-11 22:59:33 +0100

    Improve the quality of the used shell scripts
    All shell scripts have been linted with shellcheck
    to improve the quality of the scripts.

--- modules/standards/bin/generate-standards-page-by-year.sh
+++ modules/standards/bin/generate-standards-page-by-year.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
+(( $# >= 2 )) || { echo "${BASH_SOURCE[0]}: expected 2 args" >&2; return 1; }
+
 set -e -u -o pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-source  ${SCRIPT_DIR}/functions.sh
+# shellcheck disable=SC1091
+source "${SCRIPT_DIR}/functions.sh"
 
-source_file=$1
-output_file=$2
+source_file="$1"
+output_file="$2"
 
-echo > ${output_file}
-for year in $( cat ${source_file} | convert_yaml_to_json | jq .  |  jq -r ".standards.[].year" | sort -r | uniq ); do
-  echo "=== ${year}" >> ${output_file}
-  cat ${source_file} | convert_yaml_to_json | filter_by_field_and_value year ${year} | generate_list_of_links >> ${output_file}
-  echo >> ${output_file}
+echo > "${output_file}"
+for year in $( cat "${source_file}" | convert_yaml_to_json | jq .  |  jq -r '.standards.[].year' | sort -r | uniq ); do
+  {
+    echo "=== ${year}"
+    cat "${source_file}" | convert_yaml_to_json | filter_by_field_and_value year "${year}" | generate_list_of_links
+    echo
+  } >> "${output_file}"
 done
--- modules/standards/bin/sort-data-files.sh
+++ modules/standards/bin/sort-data-files.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
+(( $# >= 0 )) || { echo "${BASH_SOURCE[0]}: expected no arguments" >&2; return 1; }
+
 set -e -u -o pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-source  ${SCRIPT_DIR}/functions.sh
+# shellcheck disable=SC1091
+source "${SCRIPT_DIR}/functions.sh"
 
 tempfile=$(mktemp)
 
 cat modules/standards/data/http-related-standards.yaml \
   | yq '.standards |= sort_by(.title) ' \
-  > ${tempfile}
+  > "${tempfile}"
 
-if diff ${tempfile} modules/standards/data/http-related-standards.yaml; then
+if diff "${tempfile}" modules/standards/data/http-related-standards.yaml; then
   IS_UNSORTED=0
 else
   IS_UNSORTED=1
 fi
 
-cat ${tempfile} > modules/standards/data/http-related-standards.yaml
+cat "${tempfile}" > modules/standards/data/http-related-standards.yaml
 
-rm ${tempfile} || true
+rm "${tempfile}" || true
 
 # Soll mit <>0 aussteigen, auch wenn es sortiert wurde, damit
 # die Pipeline fehlschlägt. Es liegt in der Verantwortung
